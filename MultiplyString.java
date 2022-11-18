23d38327dadb2079aac0c3769a23f9879f00a047
Hepi Chaniyara <mahipalsinhrana078@gmail.com>
2022-11-18 12:54:17 +0530

    Comment added in Progam

--- MultiplyString.java
+++ MultiplyString.java
@@ -1,21 +1,37 @@
 import java.util.*;
 
 class MultiplyString {
-	static String multiply(String value1, String value2) {
+	private static String multiply(String value1, String value2) {
 		int len1 = value1.length();
 		int len2 = value2.length();
-		if (len1 == 0 || len2 == 0)
+		if (len1 == 0 || len2 == 0)						// If any single value is '0', then result would be '0'
 			return "0";
+		
+		//Evaluate the result based on the given input
+		int result[] = evaluateResult(value1, value2, len1, len2);
+		
+		int i = result.length - 1;
+		while (i >= 0 && result[i] == 0)
+			i--;
 
+		if (i == -1)
+		return "0";
 
-		int result[] = new int[len1 + len2];
+		String output = "";
+		
+		while (i >= 0)									// Generate the final output
+			output += (result[i--]);
 
+		return output;
+	}
+
+	private static int[] evaluateResult(String value1, String value2, int len1, int len2) {
 		int in1 = 0;
 		int in2 = 0;
-		
+		int result[] = new int[len1 + len2];
 		for (int i = len1 - 1; i >= 0; i--) {
 			int carry = 0;
-			int n1 = value1.charAt(i) - '0';
+			int n1 = value1.charAt(i) - '0';		//Fetch the exact value based on the ascii value
 			in2 = 0;
 			
 			for (int j = len2 - 1; j >= 0; j--) {
@@ -31,43 +47,48 @@ class MultiplyString {
 
 			in1++;
 		}
-		
-		int i = result.length - 1;
-		while (i >= 0 && result[i] == 0)
-		i--;
-
-		if (i == -1)
-		return "0";
-
-		String s = "";
-		
-		while (i >= 0)
-			s += (result[i--]);
-
-		return s;
+		return result;
 	}
 
 	public static void main(String[] args) {
+		Scanner scanner = null;
 		try {
-			Scanner scanner = new Scanner(System.in);
-			System.out.println("Enter first number:");
-			String str1 = scanner.nextLine();
-			System.out.println("Enter second number:");
-			String str2 = scanner.nextLine();
-	
-			if ((str1.charAt(0) == '-' || str2.charAt(0) == '-') &&
-				(str1.charAt(0) != '-' || str2.charAt(0) != '-'))
-				System.out.print("-");
-	
-			if (str1.charAt(0) == '-')
-				str1 = str1.substring(1);
-	
-			if (str2.charAt(0) == '-')
-				str2 = str2.substring(1);
-	
-			System.out.println("Multiplication of "+str1+" and "+ str2+ " is : "+multiply(str1, str2));
+			scanner = new Scanner(System.in);
+			while(true) {
+				System.out.println("1. Multiply String");
+				System.out.println("0. Exit");
+				System.out.println("Enter your choice:");				// Take choice from user
+				String choice = scanner.nextLine();
+				if("0".equals(choice)) {
+					System.out.println("Ending the program");			// Stop execution
+					break;
+				} else if("1".equals(choice)) {
+						System.out.println("Enter first number:");		// Fetch number value from user
+						String str1 = scanner.nextLine();
+						System.out.println("Enter second number:");
+						String str2 = scanner.nextLine();
+						String sign = "";
+				
+						if ((str1.charAt(0) == '-' || str2.charAt(0) == '-') &&
+							(str1.charAt(0) != '-' || str2.charAt(0) != '-'))
+							sign = "-";
+				
+						if (str1.charAt(0) == '-')
+							str1 = str1.substring(1);
+				
+						if (str2.charAt(0) == '-')
+							str2 = str2.substring(1);
+				
+						System.out.println("Multiplication is : "+sign+multiply(str1, str2)+"\n");					
+				} else {
+					System.out.println("Invalid Input");
+				}
+			}
 		} catch (Exception e) {
 			System.out.println("Exception occurred:"+e);
+		} finally {
+			if(scanner != null)
+				scanner.close();
 		}
 	}
 }
--- ReverseNumber.java
+++ ReverseNumber.java
@@ -1,21 +1,38 @@
+package com.java.practice;
 import java.util.*;
 
 class ReverseNumber {
-  public static void main(String[] args) {
-    try {
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("Enter the number:");
-		int num = scanner.nextInt();
-	    int result = 0;
-	
-	    for(;num != 0; num /= 10) {
-	      int digit = num % 10;
-	      result = result * 10 + digit;
-	    }
-	
-	    System.out.println("Result is : " + result);
-    } catch (Exception e) {
-    	System.out.println("Exception occurred:"+e);
+    public static void main(String[] args) {
+        Scanner scanner = null;
+        try {
+            scanner = new Scanner(System.in);
+
+            while(true) {
+            	System.out.println("1. Reverse Number");
+				System.out.println("0. Exit");
+				System.out.println("Enter your choice:");			//Take choice of user
+				String choice = scanner.nextLine();
+				if("0".equals(choice)) {
+					System.out.println("Ending the program");		//Stop the execution
+					break;
+				} else if("1".equals(choice)) {
+		            // Declare variable and initialize
+		            System.out.println("Enter the number:");
+		            int num = scanner.nextInt();    // Read input from the user
+		            int result = 0;
+		
+		            for(;num != 0; num /= 10) {			//Prepare the result
+		                int digit = num % 10;
+		                result = result * 10 + digit;
+		            }
+		            System.out.println("Result is : " + result);
+				}
+            }
+        } catch (Exception e) {
+            System.out.println("Exception occurred:"+e);
+        } finally {
+            if(scanner != null)
+                scanner.close(); // close the scanner object
+        }
     }
-  }
 }
